6a313d08b1a34ef904c654de442c370b16e1027e
Viacheslav <viacheslavdmytrenko03@gmail.com>
2023-12-28 23:59:13 +0100

    first start

--- Language/Program.cs
+++ Language/Program.cs
@@ -1,8 +1,23 @@
 ﻿
+using Antlr4.Runtime.Tree;
+using Antlr4.Runtime;
+
 class Program
 {
     public static void Main(string[] args)
     {
+        string input = 
+@"MODULE mod;
+    VARIABLES somevalue : INT;
+START
+    somevalue := 45;
+END mod.";
 
+        ICharStream stream = CharStreams.fromString(input);
+        ITokenSource lexer = new MyLanguageGrammarLexer(stream);
+        ITokenStream tokens = new CommonTokenStream(lexer);
+        MyLanguageGrammarParser parser = new MyLanguageGrammarParser(tokens);
+        IParseTree tree = parser.start();
+        var a = 5;
     }
 }
